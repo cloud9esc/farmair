70f94b79dadd68545cc7a05db1bd52f3c264414c
임민영 <cloud9esc@gmail.com>
2019-12-11 14:52:31 +0900

    member card without styling

--- src/component/People.tsx
+++ src/component/People.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./People.scss";
 
 import { PageTitle } from './PageTitle';
+import { MemberCard } from './MemberCard';
 
 const People = () => {
   return (
@@ -12,12 +13,41 @@ const People = () => {
             <PageTitle title="PEOPLE" />
           </div>
           <div className="col-10">
-
+            {members.map(member => (
+              <React.Fragment key={member.name}>
+                <MemberCard
+                  name={member.name}
+                  role={member.role}
+                  imageSource={member.imageSource}
+                  comment={member.comment} />
+              </React.Fragment>
+            ))}
           </div>
         </div>
       </div>
     </div>
   )
 };
+
+const members = [
+  {
+    name: "박상국",
+    role: "CEO",
+    imageSource: "/static/asset/parksanggook.jpg",
+    comment: "",
+  },
+  {
+    name: "임현진",
+    role: "Back-end Developer",
+    imageSource: "/static/asset/imhyunjin.jpeg",
+    comment: "",
+  },
+  {
+    name: "유지혜",
+    role: "Manager",
+    imageSource: "/static/asset/yoojihye.jpeg",
+    comment: "",
+  }
+]
 
 export { People };
