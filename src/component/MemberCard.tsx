f7fcce5743a9630e36a98c54ad3a22c3e5fa6a8c
임민영 <cloud9esc@gmail.com>
2019-12-12 14:47:09 +0900

    member card styling

--- src/component/MemberCard.tsx
+++ src/component/MemberCard.tsx
@@ -4,14 +4,16 @@ import "./MemberCard.scss";
 const MemberCard = (props: { name: string, role: string, imageSource: string, comment: string }) => {
   return (
     <div className="MemberCard">
-      <div className="MemberCard__image">
-        <img alt={props.name} src={props.imageSource} />
-      </div>
-      <div className="MemberCard__section">
-        <h3 className="MemberCard__name">{props.name}</h3>
-        <h4 className="MemberCard__role">{props.role}</h4>
-        <section className="MemberCard__comment">{props.comment}</section>
-      </div>
+      
+        <div className="MemberCard__image">
+          <img alt={props.name} src={props.imageSource} />
+        </div>
+        <div className="MemberCard__section">
+          <h3 className="MemberCard__name">{props.name}</h3>
+          <h4 className="MemberCard__role">{props.role}</h4>
+          <section className="MemberCard__comment">{props.comment}</section>
+        </div>
+
     </div>
   )
 }
--- src/component/People.tsx
+++ src/component/People.tsx
@@ -12,7 +12,7 @@ const People = () => {
           <div className="Service__pagetitle col-1">
             <PageTitle title="PEOPLE" />
           </div>
-          <div className="col-10">
+          <div className="col-10 Service__cards">
             {members.map(member => (
               <React.Fragment key={member.name}>
                 <MemberCard
@@ -34,19 +34,19 @@ const members = [
     name: "박상국",
     role: "CEO",
     imageSource: "/static/asset/parksanggook.jpg",
-    comment: "",
+    comment: "모든 국민은 그 보호하는 자녀에게 적어도 초등교육과 법률이 정하는 교육을 받게 할 의무를 진다.",
   },
   {
     name: "임현진",
     role: "Back-end Developer",
-    imageSource: "/static/asset/imhyunjin.jpeg",
-    comment: "",
+    imageSource: "/static/asset/imhyunjin.jpg",
+    comment: "국무총리 또는 행정각부의 장은 소관사무에 관하여 법률이나 대통령령의 위임 또는 직권으로 총리령 또는 부령을 발할 수 있다.",
   },
   {
     name: "유지혜",
     role: "Manager",
     imageSource: "/static/asset/yoojihye.jpeg",
-    comment: "",
+    comment: "모든 국민은 법 앞에 평등하다. 누구든지 성별·종교 또는 사회적 신분에 의하여 정치적·경제적·사회적·문화적 생활의 모든 영역에 있어서 차별을 받지 아니한다.",
   }
 ]
 
